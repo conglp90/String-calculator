dc39fb637fe049d8bc696b23c487f0567e948e30
conglp90 <conglp90@gmail.com>
2013-05-31 15:59:30 +0700

    abc123

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,18 +1,27 @@
+import java.awt.List;
+import java.util.ArrayList;
+
 //
 public class Calculator {
-	String deLimiter;
-	int kq=0;
-	String sohang_temp[] ;
+	
 	int Add(String numbers){
+		
+		
+		String delimiters;
+		int kq=0;
+		String sohang_temp[];
+		ArrayList<String> delimiter_list= new ArrayList<String>();
+		
 		//////tien xu ly du lieu:
 		try {
 			if(numbers.startsWith("//")){
-				deLimiter=numbers.substring(2,numbers.indexOf("\n"));
+				delimiters=numbers.substring(2,numbers.indexOf("\n"));
 				numbers=numbers.substring(numbers.indexOf("\n")+1);
+				
+//				for(String delimiter: delimiter_list)
+					numbers= numbers.replaceAll(delimiters,",");
 			}
 			numbers= numbers.replaceAll("\n",",");
-			numbers= numbers.replaceAll(deLimiter,",");
-			
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -20,10 +29,17 @@ public class Calculator {
 		//////////////////////////
 		sohang_temp=numbers.split(",");
 		try {
-			for(String sohang : sohang_temp)
+			for(String sohang : sohang_temp){
+				if(Integer.parseInt(sohang)>1000)
+					continue;
+				if(Integer.parseInt(sohang)<0)
+					throw new RuntimeException("negatives not allowed");
+				
 				kq+=Integer.parseInt(sohang);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
+			return 0;
 			// TODO: handle exception
 		}
 		return kq;
--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -46,11 +46,34 @@ public class CalculatorTest {
 	@Test
 	public void testDelimiterDefine() {
 		Calculator a= new Calculator();
-		assertTrue(a.Add("//;\n1;2")==3);
+		assertTrue(a.Add("//[;]\n1;2")==3);
 	}
+	
 	@Test
 	public void testDelimiterDefinedAndDefault() {
 		Calculator a= new Calculator();
-		assertTrue(a.Add("//;\n1;2,3\n4")==10);
+		assertTrue(a.Add("//[;]\n1;2,3\n4")==10);
 	}
+	
+	@Test
+	public void testNegativeNumber() {
+		Calculator a= new Calculator();
+		try {
+			a.Add("//;\n1;-2,3\n4");
+		} catch (Exception e) {
+			// TODO: handle exception
+			assertTrue(e.getMessage().equals("negatives not allowed"));
+		}
+	}
+	
+	@Test
+	public void testNumberBigThan1000() {
+		Calculator a= new Calculator();
+		assertTrue(a.Add("//[;]\n1;2,3000\n4")==7);
+	}
+//	@Test
+//	public void testMultiDelimiter() {
+//		Calculator a= new Calculator();
+//		assertTrue(a.Add("//[;][abc]\n1;2,3000\n4")==7);
+//	}
 }
